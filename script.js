108e8d830aafa63eda5e578dfc6274284523bc7e
sergei.obraztsov <sergei.obraztsov@sshvps.net>
2022-02-15 03:21:42 +0300

    feat: add russian jokes

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 "use strict"
 
+console.log("Шутки на русском подрубил с сайта http://www.rzhunemogu.ru/, прям не на 100% уверен что будет правильно работать")
+
 const jokeBtn=document.querySelector(".btn-joke");
 const jokeHolder=document.querySelector(".joke-holder")
 const audio = document.querySelector(".audio");
 const skeletor = document.querySelector(".mr-skeletor");
 const bubble = document.querySelector(".speech")
 const bubbleText = document.querySelector(".speech-text")
+const langBtn = document.querySelector(".btn-lang")
+
+langBtn.addEventListener("click", () => {
+  if (langBtn.dataset.lang==="en") {
+    langBtn.dataset.lang="ru"
+    jokeBtn.textContent="Хочу шутку"
+    langBtn.textContent="Скелетор, давай по английски"
+  } else {
+    langBtn.dataset.lang="en"
+    jokeBtn.textContent="I want joke"
+    langBtn.textContent="Skeletor, go russian pls"
+  }
+})
 
 let skeletorLaughtFrase = [
   "Хахаха",
@@ -25,8 +40,9 @@ skeletor.addEventListener("click", () => {
   tickles()
 });
 
-
-jokeBtn.addEventListener("click", () => jokeEvent(jokeHolder, true))
+jokeBtn.addEventListener("click", () => {
+  langBtn.dataset.lang==="en"?jokeEvent(jokeHolder, true):randomRussianJoke(jokeHolder)
+})
 
  async function jokeEvent(placeForJoke, laugth = false) {
   placeForJoke.textContent = await getData();
@@ -41,6 +57,10 @@ async function getData() {
   return data.value.joke
 }
 
+function randomRussianJoke(placeForJoke) {
+  placeForJoke.textContent=ruJokes[Math.floor(Math.random()*ruJokes.length+1)].text
+  sceletorRandomLaught()
+}
 
 
 function sceletorRandomLaught() {
@@ -60,3 +80,47 @@ function tickles() {
 }
 
 jokeEvent(jokeHolder)
+
+const ruJokes = [
+  {
+    "text": "Как называют человека, который продал свою печень? Обеспеченный.",
+  },
+  {
+    "text": "Почему шутить можно над всеми, кроме безногих? Шутки про них обычно не заходят.​",
+  },
+  {
+    "text": "Почему безногий боится гопников? Не может постоять за себя."
+  },
+  {
+    "text": "Почему толстых женщин не берут в стриптиз? Они перегибают палку."
+  },
+  {
+    "text": "Что сказал слепой, войдя в бар? 'Всем привет, кого не видел'"
+  },
+  {
+    "text": "Как называется избушка Бабы-Яги лесбиянки? Лисбушка."
+  },
+  {
+    "text": "Как предотвратить инцест у грибов? Фразой 'Не спорь с матерью!'"
+  },
+  {
+    "text": "Нервный альпинист время от времени срывается на свою жену"
+  },
+  {
+    "text": "Чего общего у некрофила и владельца строительной кампании? Они оба имеют недвижимость."
+  },
+  {
+    "text": "Почему наркоманы могут получить Нобелевскую премию по физике? Они знают как измерять скорость в граммах."
+  },
+  {
+    "text": "-Алло, это Чешская Республика? Почешите мне спинку."
+  },
+  {
+    "text": "Почему среди фигуристов, не бывает цыган? Никто не верит что это их конёк"
+  },
+  {
+    "text": "Как называется притон наркоманов-закладчиков? Клуб весёлых и находчивых."
+  },
+  {
+    "text": "В чем разница между землей и нашими шутками? Земля не плоская."
+  }]
